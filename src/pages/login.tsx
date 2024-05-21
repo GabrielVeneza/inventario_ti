e60b6fc57bc6d1eff6b76202c5f3b85c00b5ee6f
Gabriel Oliveira <goliveira@lopesrio.intra>
2024-05-21 16:25:27 -0300

    atual

---
+++ src/pages/login.tsx
@@ -0,0 +1,104 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='flex justify-center h-screen items-center'>
+    <section className="bg-white w-full h-full">
+      <div className="grid grid-cols-1 lg:grid-cols-2 h-screen">
+        <div className="relative flex items-end px-4 pb-10 pt-60 sm:pb-16 md:justify-center lg:pb-24 bg-gray-50 sm:px-6 lg:px-8">
+          <div className="absolute inset-0">
+            <img className="object-cover object-top w-full h-full" src="https://images.unsplash.com/photo-1535957998253-26ae1ef29506?auto=format&fit=crop&q=80&w=1936&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+          </div>
+          <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent"></div>
+          <div className="relative">
+            <div className="w-full max-w-xl xl:w-full xl:mx-auto xl:pr-24 xl:max-w-xl">
+              <h3 className="text-3xl font-bold text-white">Bem vindo(a) ao <br className="hidden xl:block" /><span className='text-[#ec1c4c] text-5xl'>Lopes IT</span></h3>
+              <ul className="grid grid-cols-1 mt-10 sm:grid-cols-2 gap-x-8 gap-y-4">
+                <li className="flex items-center space-x-3">
+                  <div className="inline-flex items-center justify-center flex-shrink-0 w-5 h-5 bg-[#ec1c4c] rounded-full">
+                    <svg className="w-3.5 h-3.5 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                      <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
+                    </svg>
+                  </div>
+                  <span className="text-lg font-medium text-white"> Gestão de Chamados </span>
+                </li>
+                <li className="flex items-center space-x-3">
+                  <div className="inline-flex items-center justify-center flex-shrink-0 w-5 h-5 bg-[#ec1c4c] rounded-full">
+                    <svg className="w-3.5 h-3.5 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                      <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
+                    </svg>
+                  </div>
+                  <span className="text-lg font-medium text-white"> Suporte Técnico </span>
+                </li>
+                <li className="flex items-center space-x-3">
+                  <div className="inline-flex items-center justify-center flex-shrink-0 w-5 h-5 bg-[#ec1c4c] rounded-full">
+                    <svg className="w-3.5 h-3.5 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                      <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
+                    </svg>
+                  </div>
+                  <span className="text-lg font-medium text-white"> Impressoras </span>
+                </li>
+                <li className="flex items-center space-x-3">
+                  <div className="inline-flex items-center justify-center flex-shrink-0 w-5 h-5 bg-[#ec1c4c] rounded-full">
+                    <svg className="w-3.5 h-3.5 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                      <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
+                    </svg>
+                  </div>
+                  <span className="text-lg font-medium text-white"> Redes </span>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex items-center justify-center  px-4 py-10 bg-white sm:px-6 lg:px-8 sm:py-16 lg:py-24">
+          <div className="xl:w-full xl:max-w-sm 2xl:max-w-md xl:mx-auto">
+            <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl">Digite a Senha</h2>
+            <div className="mt-8"> {/*AQUI TEM QUE SER UM FORM*/}
+              <div className="space-y-5">
+
+                <div>
+                  <div className="flex items-center justify-between">
+                    <label htmlFor="" className="text-base font-medium text-gray-900"> Senha </label>
+                  </div>
+                  <div className="mt-2.5 relative text-gray-400 focus-within:text-gray-600">
+                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                      <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M12 11c0 3.517-1.009 6.799-2.753 9.571m-3.44-2.04l.054-.09A13.916 13.916 0 008 11a4 4 0 118 0c0 1.017-.07 2.019-.203 3m-2.118 6.844A21.88 21.88 0 0015.171 17m3.839 1.132c.645-2.266.99-4.659.99-7.132A8 8 0 008 4.07M3 15.364c.64-1.319 1-2.8 1-4.364 0-1.457.39-2.823 1.07-4"
+                        />
+                      </svg>
+                    </div>
+
+                    <input
+                      itemID='login'
+                      type="password"
+                      name=""
+                      id=""
+                      placeholder="Entre com a senha"
+                      className="block w-full py-4 pl-10 pr-4 text-black placeholder-gray-500 transition-all duration-200 border border-gray-200 rounded-md bg-gray-50 focus:outline-none focus:border-blue-600 focus:bg-white caret-blue-600"
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <a href="dashboard">
+                  <button className="inline-flex items-center justify-center w-full px-4 py-4 text-base font-semibold text-white transition-all duration-200 border border-transparent rounded-md bg-gradient-to-r from-[#ec1c4c] to-blue-600 focus:outline-none hover:opacity-80 focus:opacity-80">
+                    Entrar
+                  </button>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+  )
+}
+
+export default Login
